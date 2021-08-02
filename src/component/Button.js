5892f44fae5b1c933485dbbedeee309dbb6489b1
ATPTECH\ugurc <ugurc@atp.com.tr>
2021-08-02 10:37:04 +0300

    second commit

--- src/component/Button.js
+++ src/component/Button.js
@@ -17,7 +17,7 @@ export default class Button extends React.Component {
   render() {
     const className = [
       "component-button",
-      this.props.orange ? "orange" : "",
+      this.props.orange ? "yellow" : "",
       this.props.wide ? "wide" : "",
     ];
 
