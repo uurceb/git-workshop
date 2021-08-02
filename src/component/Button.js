92253ca03d1234c9c8a3b1a95546d71196c5427f
Eray Gönülal <Minerva142@users.noreply.github.com>
2021-08-02 11:09:30 +0300

    erayray

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Button.css";
+//erayrayrayray/erayrayrayray/erayrayrayray
 
 export default class Button extends React.Component {
   static propTypes = {
